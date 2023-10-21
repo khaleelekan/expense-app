f508d4c3575828ed6aff78d949d0548bd13bb5c0
khalilullahi abdulraheem <kalilulekan@gmail.com>
2023-10-21 20:56:51 +0100

    new

--- store/expense-context.js
+++ store/expense-context.js
@@ -1,4 +1,4 @@
-import {createContext} from 'react'
+import {createContext, useReducer} from 'react'
 
 
 export const ExpenseCreateContext = createContext({
@@ -7,3 +7,45 @@ export const ExpenseCreateContext = createContext({
     deleteExpense: (id) => {},
     updateExpense: (id, {description,amount, date}) => {}
 }); 
+
+function expenseReducer (state, action){
+    switch(action.type){
+        case 'ADD':
+            const id = new Date().toString() + Math.random().toString();
+            return [{...action.payload, id: id }, ...state]
+        case 'UPDATE':
+            const updatableExpenseIndex = state.findIndex(
+                (expense) => expense.id === action.payload.id
+            );
+            const updatableExpense = state[updatableExpenseIndex];
+            const updatedItem = { ...updatableExpense, ...action.payload.data };
+            const updatedExpenses = [...state];
+            updatedExpenses[updatableExpenseIndex] = updatedItem;
+            return updatedExpenses;
+        case 'DELETE':
+            return state.filter((expense) => expense.id !== action.payload);
+        default:
+            return state;
+    }
+}
+
+function ExpenseContextProvider ({children}) {
+    const [expenseState, dispatch] = useReducer(expenseReducer);
+
+    function addExpense(expenseData){
+        dispatch({ type: 'ADD', payload: expenseData})
+    }
+
+    function deleteExpense(id){
+        dispatch({ type: 'DELETE', payload: id})
+    }
+    function updateExpense (id,expenseData){
+        dispatch({ type: 'UPDATE', payload: id , expenseData})
+    }
+
+    return <ExpenseContext.Provider>
+        {children}
+    </ExpenseContext.Provider>
+}
+
+export default ExpenseContextProvider

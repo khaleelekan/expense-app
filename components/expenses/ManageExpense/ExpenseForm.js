4c458018439b9dd60880ecc2fbe00c61a9652298
khalilullahi abdulraheem <kalilulekan@gmail.com>
2023-10-23 12:34:56 +0100

    new

--- components/expenses/ManageExpense/ExpenseForm.js
+++ components/expenses/ManageExpense/ExpenseForm.js
@@ -1,25 +1,41 @@
 import {View, StyleSheet} from 'react-native'
 import {Input} from './Input'
+import { useState } from 'react'
 
 
 export const ExpenseForm = () => {
+    const [inputValues, setInputValues] = useState({
+        amount: '',
+        date: '',
+        description: ''
+    })
 
-    function amountChangeHandler (){
-
+    function inputChangeHandler (inputIdentifier, enteredValue){
+        setInputValues((curInputValues) => {
+            return {
+                ...curInputValues,
+                [inputIdentifier]: enteredValue
+            }
+        })
     }
     return(
         <View>
             <Input label='Amount' textConfig={{
                 keyboardType: 'decimal-pad',
-                onChangeText: amountChangeHandler,
+                onChangeText: inputChangeHandler(this, 'amount') ,
+                value: inputValues.amount,
             }}/>
             <Input label='Date' textConfig={{
                 placeholder: 'YYYY-MM-DD',
                 maxLength: 10,
-                onChangeText: () => {}
+                onChangeText: inputChangeHandler(this, 'date') ,
+                value: inputValues.date,
             }}/>
-            <Input label='Description' textConfig={{
+            <Input label='Description' 
+                textConfig={{
                 mulitline: true,
+                onChangeText: inputChangeHandler(this, 'description') ,
+                value: inputValues.description,
             }}/>
         </View>
     )
